7d8a9c6117fba352555c8d5d61ea5fcb8dcf3b04
saniatariq388 <saniatariq388@gmail.com>
2025-05-16 01:58:38 +0500

    up

--- src/constant/portfolioCard.ts
+++ src/constant/portfolioCard.ts
@@ -2,33 +2,61 @@ export const portfolioCards = [
     {
         id:1,
         type:"image",
+        src:"/image/q-comm.png",
+        title:"Food Tuck Website",
+        href:"https://market-place-final-hackathon.vercel.app"
+    },
+    {
+        id:2,
+        type:"image",
+        src:"/image/blog.jpeg",
+        title:"Blog Website ",
+        href:"https://blog-website-db-4rz9.vercel.app"
+    },
+    {
+        id:3,
+        type:"image",
+        src:"/image/library.png",
+        title:"Library Management System",
+        href:"https://github.com/saniatariq388/Library_manager"
+    },
+    {
+        id:4,
+        type:"image",
         src:"/image/number.jpg",
         title:"Cli Number Guessing Game",
         href:"https://github.com/saniatariq388/CLI-NUMBER-GUESSING-GAME.git"
     },
     {
-        id:2,
+        id:5,
         type:"image",
         src:"/image/ATM.jpg",
         title:"Cli ATM machine",
         href:"https://github.com/saniatariq388/atm.git"
     },
     {
-        id:3,
+        id:6,
         type:"image",
         src:"/image/CV.PNG",
         title:"Resume Builder",
         href:"https://vercel.com/sania-tariqs-projects/resume-builder/6FGJVt9KmQ7BnyN2Ss4A9ucwDyVt"
     },
+    // {
+    //     id:4,
+    //     type:"video",
+    //     src:"/video/panacloud.mp4",
+    //     title:"Panacloud Website",
+    //     href:"https://github.com/saniatariq388/PanaCloud-website.git"
+    // },
     {
-        id:4,
-        type:"video",
-        src:"/video/panacloud.mp4",
-        title:"Panacloud Website",
-        href:"https://github.com/saniatariq388/PanaCloud-website.git"
+        id:7,
+        type:"image",
+        src:"/image/gents.png",
+        title:"AI Agentia website",
+        href:"https://ai-agentia.vercel.app"
     },
     {
-        id:5,
+        id:8,
         type:"image",
         src:"/image/cal.png",
         title:"Simple Calculator",
